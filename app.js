d8c36128a0df85af68d0eead1b6448ea4fcacd97
Arman <tysonstwrt@gmail.com>
2016-06-02 12:08:49 +0530

    Added /todos/:id

--- app.js
+++ app.js
@@ -23,6 +23,23 @@ app.get('/todos', function(req, res){
 	res.json(todos);
 });
 
+app.get('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo;
+
+	todos.forEach(function(todo){
+		if(todoId === todo.id){
+			matchedTodo = todo;
+		}
+	});
+
+	if(matchedTodo){
+		res.json(matchedTodo);
+	}else{
+		res.status(404).send();
+	}
+})
+
 
 app.listen(PORT, function(){
 	console.log('Server is listening on Port ' + PORT);
