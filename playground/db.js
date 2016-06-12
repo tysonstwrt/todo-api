a693de478ab26dd4d85c76c1ba8c4558c7396226
Arman <tysonstwrt@gmail.com>
2016-06-12 17:41:22 +0530

    Add /todo user association

--- playground/db.js
+++ playground/db.js
@@ -19,51 +19,39 @@ var Todo = sequelize.define('todo', {
 	}
 });
 
+var User = sequelize.define('user',{
+	email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
 sequelize.sync({
 	//force: true
 }).then(function(){
 	console.log('Everything is synchronized');
 
-	Todo.findById(3).then(function(todo){
-		if(todo){
-			console.log(todo.toJSON());
-		}else{
-			console.log('Todo not found');
-		}
-	}).catch(function(e){
-		console.log(e);
+	User.findById(1).then(function(user){
+		user.getTodos({
+			where: {
+				completed: false
+			}
+		}).then(function(todos){
+			todos.forEach(function(todo){
+				console.log(todo.toJSON());
+			});
+		});
 	});
-	// if(todo){
-	// 	console.log(todo.toJSON());
-	// }else{
-	// 	console.log('Unable to find todo');
-	// }
 
-	// Todo.create({
-	// 	description: 'Throw trash',
-	// 	//completed: false
+	// User.create({
+	// 	email: 'arman@example.com'
 	// }).then(function(){
 	// 	return Todo.create({
-	// 		description: 'Feed the cat'
+	// 		description: 'Clean yard'
 	// 	});
 	// }).then(function(todo){
-	// 	//return Todo.findById(1);
-	// 	return Todo.findAll({
-	// 		where: {
-	// 			description: {
-	// 				$like: '%feed%'
-	// 			}
-	// 		}
+	// 	User.findById(1).then(function(user){
+	// 		user.addTodo(todo);
 	// 	});
-	// }).then(function(todos){
-	// 	if(todos){
-	// 		todos.forEach(function(todo){
-	// 			console.log(todo.toJSON());
-	// 		});
-	// 	}else{
-	// 		console.log('No todo found!');
-	// 	}
-	// }).catch(function(e){
-	// 	console.log(e);
 	// });
 });
